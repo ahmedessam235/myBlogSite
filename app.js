5826385168a239c440f4b5a8e467446fff89c487
ahmedessam235 <ahmedessameltantawy@gmail.com>
2021-02-21 17:11:04 +0200

    adding port conigurations for heroku

--- app.js
+++ app.js
@@ -63,16 +63,6 @@ if (titleInput === storedTitle){
 });
 });
 
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(process.env.PORT||3000, function() {
   console.log("Server started on port 3000");
 });
